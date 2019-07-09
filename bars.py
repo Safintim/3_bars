b3d0503d8f110dd6e813e3d2a0e4f73de57b01c5
Safin Timur <timurtlt96@mail.ru>
2019-07-09 11:52:23 +0300

    Removed same code

--- bars.py
+++ bars.py
@@ -7,23 +7,23 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bars):
-    biggest_bar = max(bars['features'], key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    biggest_bar = max(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
     return biggest_bar['properties']['Attributes']['Name']
 
 
 def get_smallest_bar(bars):
-    smallest_bar = min(bars['features'], key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    smallest_bar = min(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
     return smallest_bar['properties']['Attributes']['Name']
 
 
 def get_closest_bar(bars, longitude, latitude):
-    nearest_bar = min(bars['features'], key=lambda bar: (abs(bar['geometry']['coordinates'][0]-longitude),
-                                                         abs(bar['geometry']['coordinates'][1]-latitude)))
+    nearest_bar = min(bars, key=lambda bar: (abs(bar['geometry']['coordinates'][0]-longitude),
+                                             abs(bar['geometry']['coordinates'][1]-latitude)))
     return nearest_bar['properties']['Attributes']['Name']
 
 
 def main():
-    bars = load_data('bars.json')
+    bars = load_data('bars.json')['features']
     print('Самый большой бар: {}'.format(get_biggest_bar(bars)))
     print('Самый маленький бар: {}'.format(get_smallest_bar(bars)))
 
